51abfb594267655be1e88c09965e4105449ccfd7
Afraym-Herz <afraym100herz@gmail.com>
2025-07-19 12:31:45 +0300

    create on_boarding view with its route

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,22 @@
+import 'package:e_commerce/core/helper_functions/on_generate_routes.dart';
+import 'package:e_commerce/features/splash/presentation/views/splash_view.dart';
 import 'package:e_commerce/generated/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const FruitHub());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FruitHub extends StatelessWidget {
+  const FruitHub({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     localizationsDelegates: [
-                S.delegate,
-                GlobalMaterialLocalizations.delegate,
-                GlobalWidgetsLocalizations.delegate,
-                GlobalCupertinoLocalizations.delegate,
-            ],
+      onGenerateRoute: onGenerateRoutes,
+      initialRoute: SplashView.routeName,
+     home: SplashView(),
     );
   }
 }
