cd25d7211f2cef640c6f34f74fb11fd33702965a
Afraym-Herz <afraym100herz@gmail.com>
2025-07-19 12:30:31 +0300

    change app icon

---
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,39 @@
+import 'package:e_commerce/core/utils/assets.dart';
+import 'package:e_commerce/features/on_boarding/presentation/views/on_boarding_view.dart';
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+
+@override
+  void initState() {
+    executeNavigation();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image.asset(Assets.imagesPlanet),
+        Image.asset(Assets.imagesAppIcon),
+        Image.asset(Assets.imagesSplashBottom),
+        
+      ],
+    );
+  }
+  
+  void executeNavigation() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacementNamed(context, OnBoardingView.routeName);
+    });
+  }
+}
