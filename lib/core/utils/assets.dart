4328184d25c7cc76be9c1fda31a14e8d0c77107f
Afraym-Herz <afraym100herz@gmail.com>
2025-07-19 12:31:21 +0300

    create splash view and splash view body

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAppIcon
+  /// assets/images/app_icon.png
+  static const String imagesAppIcon = "assets/images/app_icon.png";
+
+  /// Assets for imagesPlanet
+  /// assets/images/planet.png
+  static const String imagesPlanet = "assets/images/planet.png";
+
+  /// Assets for imagesSplashBottom
+  /// assets/images/splash_bottom.png
+  static const String imagesSplashBottom = "assets/images/splash_bottom.png";
+}
+
