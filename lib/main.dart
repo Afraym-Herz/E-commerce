4fe804384179966b2199a1c0444ebe5831116b64
Afraym-Herz <afraym100herz@gmail.com>
2025-07-30 23:05:41 +0300

    create errors folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:e_commerce/core/helper_functions/on_generate_routes.dart';
 import 'package:e_commerce/core/services/shared_preferences_singelton.dart';
 import 'package:e_commerce/features/splash/presentation/views/splash_view.dart';
+import 'package:e_commerce/firebase_options.dart';
 import 'package:e_commerce/generated/l10n.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:intl/intl.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await Prefs.init();
   runApp(const FruitHub());
 }
@@ -18,9 +22,7 @@ class FruitHub extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        fontFamily: 'Cairo',
-      ),
+      theme: ThemeData(fontFamily: 'Cairo'),
       localizationsDelegates: const [
         S.delegate,
         GlobalMaterialLocalizations.delegate,
@@ -36,8 +38,6 @@ class FruitHub extends StatelessWidget {
   }
 }
 
-
-bool isArabic (){
+bool isArabic() {
   return Intl.getCurrentLocale() == 'ar';
 }
-
