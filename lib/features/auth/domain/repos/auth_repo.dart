f16ba7ca88b9cf27eabf97a807ffecfec83ef5a7
Afraym-Herz <afraym100herz@gmail.com>
2025-07-30 23:12:21 +0300

    create user entity and abstract auth repo

---
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_commerce/core/errors/failures.dart';
+import 'package:e_commerce/features/auth/domain/entity/user_entity.dart';
+
+abstract class AuthRepo {
+  Future<Either<Failures,UserEntity>> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
+}
