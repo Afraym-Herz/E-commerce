7528722081d78a75af3354ad4951ec70dadb729d
Afraym-Herz <afraym100herz@gmail.com>
2025-07-31 23:47:13 +0300

    create custom bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce/core/helper_functions/on_generate_routes.dart';
+import 'package:e_commerce/core/services/custom_bloc_observer.dart';
 import 'package:e_commerce/core/services/get_it_services.dart';
 import 'package:e_commerce/core/services/shared_preferences_singelton.dart';
 import 'package:e_commerce/features/splash/presentation/views/splash_view.dart';
@@ -6,12 +7,14 @@ import 'package:e_commerce/firebase_options.dart';
 import 'package:e_commerce/generated/l10n.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:intl/intl.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  Bloc.observer = CustomBlocObserver();
   setupGetIt(); 
   await Prefs.init();
   runApp(const FruitHub());
