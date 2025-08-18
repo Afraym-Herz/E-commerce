ab327deb63fb8900cb6b375da801cee60d449f48
Afraym-Herz <afraym100herz@gmail.com>
2025-08-18 18:09:50 +0300

    add cubits methods in best seller view and make it individual view

--- lib/features/home/presentation/views/widgets/best_seller_view_body.dart
+++ lib/features/home/presentation/views/widgets/best_seller_view_body.dart
@@ -1,31 +1,47 @@
-import 'package:e_commerce/core/entities/product_entity.dart';
+import 'package:e_commerce/core/cubits/product_cubit/products_cubit.dart';
 import 'package:e_commerce/core/utils/app_text_styles.dart';
-import 'package:e_commerce/features/home/presentation/views/widgets/custom_grid_view_fruits_items.dart';
+import 'package:e_commerce/features/home/presentation/views/widgets/grid_view_fruit_item_bloc_builder.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class BestSellerViewBody extends StatelessWidget {
-  const BestSellerViewBody({super.key, required this.products});
-  final List<ProductEntity> products ;
+class BestSellerViewBody extends StatefulWidget {
+  const BestSellerViewBody({super.key});
+
+  @override
+  State<BestSellerViewBody> createState() => _BestSellerViewBodyState();
+}
+
+class _BestSellerViewBodyState extends State<BestSellerViewBody> {
+
+  @override
+  void initState() {
+    context.read<ProductsCubit>().getProducts() ;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  CustomScrollView(
+    return const CustomScrollView(
       slivers: [
-        const SliverToBoxAdapter(child: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.only( right: 12,  left: 12,),
-          child: Column(
-            children: [
-              SizedBox(height: 25,),
-              Align(alignment: Alignment.centerRight, child: Text('الاكثر مبيعًا', style: AppTextStyles.bold19,)),
-            ],
+        SliverToBoxAdapter(
+          child: SafeArea(
+            child: Padding(
+              padding: EdgeInsets.only(right: 12, left: 12),
+              child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.center,
+                    child: Text('الاكثر مبيعًا', style: AppTextStyles.bold19),  
+                  ),SizedBox(height: 25),
+                ],
+              ),
+            ),
           ),
         ),
-      ),
-      ),       
-      CustomGridViewFruitsItems(
-        productsList: products,
-      ),
+        GridViewFruitItemBlocBuilder(),
       ],
     );
   }
 }
+
+
