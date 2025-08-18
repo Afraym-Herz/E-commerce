94e20eb2f41ab2cd36fc710aab7112dc13d75238
Afraym-Herz <afraym100herz@gmail.com>
2025-08-18 21:45:06 +0300

    make main view is contain several screens

--- lib/features/home/presentation/views/main_view.dart
+++ lib/features/home/presentation/views/main_view.dart
@@ -1,16 +1,42 @@
-import 'package:e_commerce/features/home/presentation/views/best_seller_view.dart';
+import 'package:e_commerce/features/home/presentation/views/cart_view.dart';
+import 'package:e_commerce/features/home/presentation/views/products_view.dart';
+import 'package:e_commerce/features/home/presentation/views/profile_view.dart';
 import 'package:e_commerce/features/home/presentation/views/widgets/custom_bottom_navigation_bar.dart';
+import 'package:e_commerce/features/home/presentation/views/widgets/home_view.dart';
 import 'package:flutter/material.dart';
 
-class MainView extends StatelessWidget {
+class MainView extends StatefulWidget {
    const MainView({super.key});
   static const routeName = 'main';
-  
+
+  @override
+  State<MainView> createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> {
+
+  final List<Widget> screens = [
+    const HomeView(),
+    const ProductsView(),
+    const CartView(),
+    const ProfileView(),
+  ];
+
+
+ int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(child: BestSellerView()),
-      bottomNavigationBar: CustomBottomNavigationBar(),
+    return  Scaffold(
+      bottomNavigationBar: CustomBottomNavigationBar(
+        onItemTapped: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        index: currentIndex ,
+      ),
+      body: SafeArea(child: screens[currentIndex]),
+      
     );
   }
 }
