83b4c0eb37bc68548bc74f1cfb8dbc4d0e072738
Afraym-Herz <afraym100herz@gmail.com>
2025-08-18 18:09:02 +0300

    make custom grid view fruits items accept items from internet only

--- lib/features/home/presentation/views/widgets/custom_grid_view_fruits_items.dart
+++ lib/features/home/presentation/views/widgets/custom_grid_view_fruits_items.dart
@@ -1,37 +1,25 @@
 import 'package:e_commerce/core/entities/product_entity.dart';
-import 'package:e_commerce/core/utils/assets.dart';
 import 'package:e_commerce/core/widgets/fruit_item.dart';
 import 'package:flutter/material.dart';
 
 class CustomGridViewFruitsItems extends StatelessWidget {
-  const CustomGridViewFruitsItems({super.key, this.productsList});
+  const CustomGridViewFruitsItems({super.key, required this.productsList});
+  final List<ProductEntity>? productsList;
 
-  final List<ProductEntity>? productsList ;
-
-  // final List<FruitItem> fruitsList = const [
-  //   FruitItem(fruitImage: Assets.imagesWatermelon , fruitName: 'بطيخ' , fruitPrice: '30' , ),
-  //   FruitItem(fruitImage: Assets.imagesStrawberry , fruitName: 'فراولة' , fruitPrice: '200' , ),
-  //   FruitItem(fruitImage: Assets.imagesAvocado , fruitName: 'أفوكادو' , fruitPrice: '200' , ),
-  //   FruitItem(fruitImage: Assets.imagesPineappleFruit , fruitName: 'أناناس' , fruitPrice: '20' , ),
-  //   FruitItem(fruitImage: Assets.imagesWatermelon , fruitName: 'بطيخ' , fruitPrice: '30' , ),
-  //   FruitItem(fruitImage: Assets.imagesStrawberry , fruitName: 'فراولة' , fruitPrice: '200' , ),
-  //   FruitItem(fruitImage: Assets.imagesAvocado , fruitName: 'أفوكادو' , fruitPrice: '200' , ),
-  //   FruitItem(fruitImage: Assets.imagesPineappleFruit , fruitName: 'أناناس' , fruitPrice: '20' , ),
-  // ];
 
   @override
   Widget build(BuildContext context) {
     return SliverGrid.builder(
-      gridDelegate: 
-    const SliverGridDelegateWithFixedCrossAxisCount(
-      crossAxisCount: 2,
-      mainAxisSpacing: 8,
-      crossAxisSpacing: 16 ,
-      childAspectRatio: 163/214,
-    ) , 
-    itemCount: productsList != null ? productsList!.length : 8 , 
-     itemBuilder: (context , index ){
-      return FruitItem(productEntity:  productsList![index] ,);  
-    }) ;
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisSpacing: 8,
+        crossAxisSpacing: 16,
+        childAspectRatio: 163 / 214,
+      ),
+      itemCount: productsList!.length ,
+      itemBuilder: (context, index) {
+      return FruitItem(productEntity: productsList![index]);
+      },
+    );
   }
 }
---
+++ lib/features/home/presentation/views/widgets/grid_view_fruit_item_bloc_builder.dart
@@ -0,0 +1,41 @@
+import 'dart:developer';
+
+import 'package:e_commerce/core/cubits/product_cubit/products_cubit.dart';
+import 'package:e_commerce/core/helper_functions/get_dummy_product.dart';
+import 'package:e_commerce/core/widgets/custom_error_widget.dart';
+import 'package:e_commerce/features/home/presentation/views/widgets/custom_grid_view_fruits_items.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+
+class GridViewFruitItemBlocBuilder extends StatelessWidget {
+  const GridViewFruitItemBlocBuilder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ProductsCubit, ProductsState>(
+      builder: (context, state) {
+        if (state is ProductsSuccess) {
+          log('is product success ${state.products.length.toString()}');
+          return  CustomGridViewFruitsItems(
+            productsList: state.products ,
+          );
+        } else if (state is ProductsFailure) {
+          return Center(child: Text(state.message));
+        } else if (state is ProductsLoading) {
+          log('is product loading ');
+          return Skeletonizer.sliver(
+            enabled: true,
+            child:  CustomGridViewFruitsItems(
+              productsList: getDummyProducts() ,
+            ),
+          );
+          
+        } else {
+            log('is in else scope');
+            return const SliverToBoxAdapter(child: CustomErrorWidget(message: 'is in else scope'));
+          }
+      },
+    );
+  }
+}
