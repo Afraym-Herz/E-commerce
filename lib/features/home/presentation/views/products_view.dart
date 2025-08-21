44649b5564d8aa5377130ab43d6ae86d5e8eeb6b
Afraym-Herz <afraym100herz@gmail.com>
2025-08-21 23:47:43 +0300

    create and provider bloc to products view

--- lib/features/home/presentation/views/products_view.dart
+++ lib/features/home/presentation/views/products_view.dart
@@ -1,10 +1,20 @@
+import 'package:e_commerce/core/cubits/product_cubit/products_cubit.dart';
+import 'package:e_commerce/core/repos/product_repo.dart';
+import 'package:e_commerce/core/services/get_it_services.dart';
+import 'package:e_commerce/features/home/presentation/views/widgets/products_view_bloc_builder.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ProductsView extends StatelessWidget {
   const ProductsView({super.key});
 
+  static const routeName = 'products';
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BlocProvider(
+      create: (context) => ProductsCubit(productRepo: getIt<ProductRepo>()),
+       child: const ProductsViewBlocBuilder(),
+    );
   }
-}
+}     
---
+++ lib/features/home/presentation/views/widgets/products_view_bloc_builder.dart
@@ -0,0 +1,42 @@
+
+import 'dart:developer';
+
+import 'package:e_commerce/core/cubits/product_cubit/products_cubit.dart';
+import 'package:e_commerce/core/helper_functions/get_dummy_product.dart';
+import 'package:e_commerce/core/widgets/custom_error_widget.dart';
+import 'package:e_commerce/features/home/presentation/views/widgets/products_view_body.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+
+class ProductsViewBlocBuilder extends StatelessWidget {
+  const ProductsViewBlocBuilder({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ProductsCubit, ProductsState>(
+      builder: (context, state) {
+        if (state is ProductsSuccess) {
+          return  ProductsViewBody(
+            productsList: state.products  ,
+          );
+        }          
+        else if (state is ProductsFailure) {
+        return  CustomErrorWidget(message: state.message);
+        }
+        else {
+          return Skeletonizer(
+            enabled: state is ProductsLoading,
+            child: ProductsViewBody(
+              productsList: getDummyProducts() ,
+            ),
+          );
+        }
+        
+        
+      },
+    );
+  }
+}
