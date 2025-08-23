91a3c8b95c3ee977b53bd4c4e07f19ffb2f2dbff
Afraym-Herz <afraym100herz@gmail.com>
2025-08-23 19:58:13 +0300

    create products view header and edit in horizontail text widget

--- lib/core/widgets/products_view_header.dart
+++ lib/core/widgets/products_view_header.dart
@@ -3,17 +3,18 @@ import 'package:e_commerce/core/utils/app_text_styles.dart';
 import 'package:e_commerce/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class HTextWidget extends StatelessWidget {
-  const HTextWidget({
+class ProductsViewHeader extends StatelessWidget {
+  const ProductsViewHeader({
     super.key,
-    required this.title,
+    required this.title, required this.productsLength,
   });
   final String title;
+  final int productsLength;
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Text(title, style: AppTextStyles.bold16),
+        Text('$productsLength نتائج', style: AppTextStyles.bold16),
         const Spacer(),
         GestureDetector(
           onTap: (){},
