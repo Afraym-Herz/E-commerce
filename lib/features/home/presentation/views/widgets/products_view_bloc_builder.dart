e10d8a65e25a59f00f1d5b1ec9bd99d1f48ac6a6
Afraym-Herz <afraym100herz@gmail.com>
2025-08-23 19:16:26 +0300

    create products view body before filters

--- lib/features/home/presentation/views/widgets/products_view_bloc_builder.dart
+++ lib/features/home/presentation/views/widgets/products_view_bloc_builder.dart
@@ -5,6 +5,7 @@ import 'package:e_commerce/core/cubits/product_cubit/products_cubit.dart';
 import 'package:e_commerce/core/helper_functions/get_dummy_product.dart';
 import 'package:e_commerce/core/widgets/custom_error_widget.dart';
 import 'package:e_commerce/features/home/presentation/views/widgets/products_view_body.dart';
+import 'package:e_commerce/features/home/presentation/views/widgets/products_view_body_results.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:skeletonizer/skeletonizer.dart';
@@ -19,7 +20,7 @@ class ProductsViewBlocBuilder extends StatelessWidget {
     return BlocBuilder<ProductsCubit, ProductsState>(
       builder: (context, state) {
         if (state is ProductsSuccess) {
-          return  ProductsViewBody(
+          return  ProductsViewBodyResults(
             productsList: state.products  ,
           );
         }          
@@ -29,7 +30,7 @@ class ProductsViewBlocBuilder extends StatelessWidget {
         else {
           return Skeletonizer(
             enabled: state is ProductsLoading,
-            child: ProductsViewBody(
+            child: ProductsViewBodyResults(
               productsList: getDummyProducts() ,
             ),
           );
--- lib/features/home/presentation/views/widgets/products_view_body_results.dart
+++ lib/features/home/presentation/views/widgets/products_view_body_results.dart
@@ -2,31 +2,29 @@ import 'package:e_commerce/core/cubits/product_cubit/products_cubit.dart';
 import 'package:e_commerce/core/entities/product_entity.dart';
 import 'package:e_commerce/core/widgets/custom_app_bar.dart';
 import 'package:e_commerce/core/widgets/custom_search_text_field.dart';
-import 'package:e_commerce/core/widgets/h_text_row.dart';
 import 'package:e_commerce/core/widgets/h_text_widget.dart';
-import 'package:e_commerce/features/home/presentation/views/best_seller_view.dart';
+import 'package:e_commerce/core/widgets/products_view_header.dart';
 import 'package:e_commerce/features/home/presentation/views/widgets/custom_grid_view_fruits_items.dart';
-import 'package:e_commerce/features/home/presentation/views/widgets/custom_product_fruit_item_list_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ProductsViewBody extends StatefulWidget {
-  const ProductsViewBody({super.key, required this.productsList});
+class ProductsViewBodyResults extends StatefulWidget {
+  const ProductsViewBodyResults({super.key, required this.productsList});
+    final List<ProductEntity> productsList ;
 
-  final List<ProductEntity> productsList ;
 
   @override
-  State<ProductsViewBody> createState() => _ProductsViewBodyState();
+  State<ProductsViewBodyResults> createState() => _ProductsViewBodyResultsState();
 }
 
-class _ProductsViewBodyState extends State<ProductsViewBody> {
+class _ProductsViewBodyResultsState extends State<ProductsViewBodyResults> {
 
   @override
   void initState() {
     context.read<ProductsCubit>().getProducts();
     super.initState();
   }
-      
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -40,21 +38,8 @@ class _ProductsViewBodyState extends State<ProductsViewBody> {
                 const SizedBox(height: 20),
                 CustomSearchTextField(onSaved: (value) {}),
                 const SizedBox(height: 20),
-                const HTextWidget( title: 'منتجاتنا'),
+                ProductsViewHeader( title: 'نتائج', productsLength: context.read<ProductsCubit>().productsLength ,),
                 const SizedBox(height: 20),
-                 CustomProductFruitItemListView(
-                  productsEntity: widget.productsList ,
-                ),
-                const SizedBox(height: 20),
-                HTextRow(
-                  onPressed: () {
-                    Navigator.pushNamed(context, BestSellerView.routeName);
-                  },
-                  title: 'الأكثر مبيعًا',
-                  lastText: 'المزيد',
-                ),
-                const SizedBox(height: 20),
-                
               ],
             ),
           ),
