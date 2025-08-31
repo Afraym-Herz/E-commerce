f23dd832e07f1186dd64d3bb6ac03c039126dc8a
Afraym-Herz <afraym100herz@gmail.com>
2025-08-31 15:37:25 +0300

    create cart entity and edit in cart item entity

---
+++ lib/features/home/domain/entites/cart_entity.dart
@@ -0,0 +1,30 @@
+import 'package:e_commerce/core/entities/product_entity.dart';
+import 'package:e_commerce/features/home/domain/entites/cart_item_entity.dart';
+
+class CartEntity {
+  final List<CartItemEntity> cartItems;
+  CartEntity({required this.cartItems});
+
+  addCartItemEntity(CartItemEntity cartItemEntity) {
+    cartItems.add(cartItemEntity);
+  }
+
+  bool productExists(ProductEntity productEntity) {
+    for (CartItemEntity cartItemEntity in cartItems) {
+      if (cartItemEntity.productEntity.productName == productEntity.productName) {
+       return true;
+      }
+    }
+    return false;
+  }
+
+  CartItemEntity getCartItem(ProductEntity productEntity) {
+    for (CartItemEntity cartItemEntity in cartItems) {
+      if (productEntity == cartItemEntity.productEntity) {
+        return cartItemEntity;
+      }
+    }
+    return CartItemEntity(productEntity: productEntity);
+  }
+
+}
--- lib/features/home/domain/entites/cart_item_entity.dart
+++ lib/features/home/domain/entites/cart_item_entity.dart
@@ -3,14 +3,13 @@ import 'package:e_commerce/core/entities/product_entity.dart';
 class CartItemEntity {
   final ProductEntity productEntity;
   int count;
-  CartItemEntity({required this.productEntity, this.count = 0});
+  CartItemEntity({required this.productEntity, this.count = 1});
 
   calcTotalPrice() {
     return productEntity.productPrice * count;
   }
   
   incrementCount() => count++;
-  
   decrementCount() => count--;
   
 }
