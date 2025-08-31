ccacdda1c390db1463a5c8fa7c08d50031f05a0a
Afraym-Herz <afraym100herz@gmail.com>
2025-08-31 16:57:47 +0300

    edit cart items sliver list view

--- lib/features/home/presentation/views/widgets/cart_view_body.dart
+++ lib/features/home/presentation/views/widgets/cart_view_body.dart
@@ -2,39 +2,41 @@ import 'package:e_commerce/core/utils/app_text_styles.dart';
 import 'package:e_commerce/core/widgets/custom_app_bar.dart';
 import 'package:e_commerce/core/widgets/custom_button.dart';
 import 'package:e_commerce/core/widgets/custom_divider.dart';
-import 'package:e_commerce/features/home/presentation/views/widgets/cart_item.dart';
+import 'package:e_commerce/features/home/presentation/manager/cart_cubit/cart_cubit.dart';
 import 'package:e_commerce/features/home/presentation/views/widgets/cart_items_sliver_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CartViewBody extends StatelessWidget {
   const CartViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Stack(
+    return Stack(
       children: [
         CustomScrollView(
           slivers: [
             SliverToBoxAdapter(
               child: Column(
                 children: [
-                 customAppBar(context, title: 'السلة', hasNotif: false,) ,
-                 const SizedBox(height: 20),
-                 Container(
-                  height: 41,
-                  decoration: const BoxDecoration(
-                    color: Color(0xffEBF9F1)
+                  customAppBar(context, title: 'السلة', hasNotif: false),
+                  const SizedBox(height: 20),
+                  Container(
+                    height: 41,
+                    decoration: const BoxDecoration(color: Color(0xffEBF9F1)),
+                    child: Center(
+                      child: Text(
+                        'لديك ${context.watch<CartCubit>().cartEntity.cartItems.length} منتجات في سله التسوق',
+                        style: AppTextStyles.regular13,
+                      ),
+                    ),
                   ),
-                  child: const Center(
-                    child: Text('لديك 3 منتجات في سله التسوق', style: AppTextStyles.regular13 ,),
-                  )
-                 ),
-                 const SizedBox(height: 20),
-                 const CustomDivider(),
+                  const SizedBox(height: 20),
+                  const CustomDivider(),
                 ],
               ),
             ),
-             CartItemsSliverListView(),
+            CartItemsSliverListView(),
           ],
         ),
         Positioned(
@@ -42,11 +44,11 @@ class CartViewBody extends StatelessWidget {
           right: 0,
           bottom: MediaQuery.of(context).size.height * 0.01,
           child: Padding(
-            padding: const EdgeInsets.symmetric( horizontal: 18),
-            child: CustomButton(onPressed: (){}, title: 'الدفع  180جنيه'),
-          ))
+            padding: const EdgeInsets.symmetric(horizontal: 18),
+            child: CustomButton(onPressed: () {}, title: 'الدفع  180جنيه'),
+          ),
+        ),
       ],
     );
   }
 }
-
--- lib/features/home/presentation/views/widgets/home_view_body_bloc_builder.dart
+++ lib/features/home/presentation/views/widgets/home_view_body_bloc_builder.dart
@@ -2,7 +2,7 @@ import 'package:e_commerce/core/cubits/product_cubit/products_cubit.dart';
 import 'package:e_commerce/core/helper_functions/get_dummy_product.dart';
 import 'package:e_commerce/core/widgets/custom_error_widget.dart';
 import 'package:e_commerce/features/home/presentation/views/widgets/home_view_body.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
