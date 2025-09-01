d314bfaace2cc033056a30096191d87244571b17
Afraym-Herz <afraym100herz@gmail.com>
2025-09-01 13:19:09 +0300

    make cart items sliver list get their childeren from list

--- lib/features/home/presentation/views/widgets/cart_items_sliver_list_view.dart
+++ lib/features/home/presentation/views/widgets/cart_items_sliver_list_view.dart
@@ -1,20 +1,22 @@
 import 'package:e_commerce/core/widgets/custom_divider.dart';
+import 'package:e_commerce/features/home/domain/entites/cart_entity.dart';
 import 'package:e_commerce/features/home/presentation/views/widgets/cart_item.dart';
 import 'package:flutter/material.dart';
 
 class CartItemsSliverListView extends StatelessWidget {
-   CartItemsSliverListView({super.key});
+   const CartItemsSliverListView({super.key, required this.cartEntity});
 
-  final List<CartItem> cartItems = [];
+  final CartEntity cartEntity;
 
   @override
   Widget build(BuildContext context) {
     return SliverList.separated(
       separatorBuilder: (context, index) => const CustomDivider(),
-      itemBuilder: (context, index) => index == cartItems.length - 1
-          ? Column(children: [cartItems[index], const CustomDivider()])
-          : cartItems[index],
-      itemCount: cartItems.length,
+      itemBuilder: (context, index) => index == cartEntity.cartItems.length - 1
+          ? Column(children: [CartItem(cartItemEntity: cartEntity.cartItems[index]), const CustomDivider()])
+          : CartItem(cartItemEntity: cartEntity.cartItems[index],
+      ),
+      itemCount: cartEntity.cartItems.length,
     );
   }
 }
