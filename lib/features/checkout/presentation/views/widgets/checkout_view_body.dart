a905a1790599055d003b6ca4ad556d176d4e385d
Afraym-Herz <afraym100herz@gmail.com>
2025-09-04 20:06:08 +0300

    convert checkout view body to stack and add next step button

--- lib/features/checkout/presentation/views/widgets/checkout_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/checkout_view_body.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 import 'package:e_commerce/core/widgets/custom_app_bar.dart';
 import 'package:e_commerce/core/widgets/custom_button.dart';
 import 'package:e_commerce/features/checkout/presentation/views/widgets/custom_checkout_app_bar.dart';
+import 'package:e_commerce/features/checkout/presentation/views/widgets/shipping_body.dart';
 import 'package:e_commerce/features/checkout/presentation/views/widgets/step_items_list.dart';
 import 'package:flutter/material.dart';
 
@@ -19,48 +20,72 @@ class _CheckoutViewBodyState extends State<CheckoutViewBody> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+      child: Stack(
         children: [
-          customCheckoutAppBar(context, title: stepItemsTitles[currentIndex] , 
-          onPressed: (){
-            currentIndex--;
-            log('currentIndex is decresed $currentIndex');
-            setState(() {});
-          },
-          ) ,
-          const SizedBox(height: 20),
-          StepItemsList(currentIndex: currentIndex),
-          const SizedBox(height: 20),
-          stepItemsBodies[currentIndex],
-
-          CustomButton(
-            onPressed: () {
-              if (currentIndex < stepItemsBodies.length - 1) {
-                currentIndex++;
-                setState(() {});
-              } else {
-                log('it is end of steps');
-              }
-            },
-            title: buttonTitles[currentIndex],
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              customCheckoutAppBar(
+                context,
+                title: stepItemsTitles[currentIndex],
+                onPressed: () {
+                  currentIndex--;
+                  log('currentIndex is decresed $currentIndex');
+                  setState(() {});
+                },
+              ),
+              const SizedBox(height: 20),
+              StepItemsList(currentIndex: currentIndex),
+              const SizedBox(height: 20),
+              stepItemsBodies[currentIndex],
+            ],
           ),
+          currentIndex == 0
+              ? Positioned(
+                  bottom: 400,
+                  left: 0,
+                  right: 0,
+                child: CustomButton(
+                    onPressed: () {
+                      if (currentIndex < stepItemsBodies.length - 1) {
+                        currentIndex++;
+                        setState(() {});
+                      } else {
+                        log('it is end of steps');
+                      }
+                    },
+                    title: buttonTitles[currentIndex],
+                  ),
+              )
+              : Positioned(
+                  bottom: 200,
+                  left: 0,
+                  right: 0,
+                  child: CustomButton(
+                    onPressed: () {
+                      if (currentIndex < stepItemsBodies.length - 1) {
+                        currentIndex++;
+                        setState(() {});
+                      } else {
+                        log('it is end of steps');
+                      }
+                    },
+                    title: buttonTitles[currentIndex],
+                  ),
+                ),
         ],
       ),
     );
   }
 
   final List<Widget> stepItemsBodies = [
-    const Placeholder(),
+    const ShippingBody(),
     const SizedBox(
       width: 50,
       height: 50,
       child: Center(child: Text('العنوان')),
     ),
-    const SizedBox(
-      width: 50,
-       height: 50,
-        child: Center(child: Text('الدفع'))),
+    const SizedBox(width: 50, height: 50, child: Center(child: Text('الدفع'))),
     const SizedBox(
       width: 50,
       height: 50,
