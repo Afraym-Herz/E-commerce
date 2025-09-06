c42a1a8cb62798f65a79158ac736632e613bed43
Afraym-Herz <afraym100herz@gmail.com>
2025-09-06 11:20:56 +0300

    fix issues with custom checkout app bar

--- lib/features/checkout/presentation/views/widgets/custom_checkout_app_bar.dart
+++ lib/features/checkout/presentation/views/widgets/custom_checkout_app_bar.dart
@@ -1,18 +1,30 @@
+import 'dart:developer';
+
 import 'package:e_commerce/core/utils/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
-AppBar customCheckoutAppBar(BuildContext context , {required String title ,required VoidCallback onPressed }) =>  AppBar(
-    title:  Text(title, style: AppTextStyles.bold19),
-    centerTitle: true,
-    scrolledUnderElevation: 0,
-    surfaceTintColor: Colors.white,
-    leading: IconButton(
-    onPressed:onPressed,
-    
-    icon: const Icon(
-      Icons.arrow_back_ios_rounded,
-      color: Color(0xff000000),
-    ),
-    ),
+AppBar customCheckoutAppBar(
+  BuildContext context, {
+  required int currentIndex,
+  VoidCallback? onPressed,
+}) => AppBar(
+  title: Text(buttonTitles[currentIndex], style: AppTextStyles.bold19),
+  centerTitle: true,
+  backgroundColor: Colors.white,
+  scrolledUnderElevation: 0,
+  surfaceTintColor: Colors.white,
+  leading: IconButton(
+    onPressed: onPressed ?? () {
+            log(currentIndex.toString());
+            Navigator.of(context).pop();
+          },
+    icon: const Icon(Icons.arrow_back_ios_rounded, color: Color(0xff000000)),
+  ),
+);
 
-  );
+const List<String> buttonTitles = [
+  'التالي',
+  'التالي',
+  'تأكيد & استمرار',
+  'تأكيد الطلب',
+];
