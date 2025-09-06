b8a4ddb7bf1f9fc95b05a505b655202ccc2198b6
Afraym-Herz <afraym100herz@gmail.com>
2025-09-06 13:18:13 +0300

    edit payment item container

--- lib/features/checkout/presentation/views/widgets/payment_item_container.dart
+++ lib/features/checkout/presentation/views/widgets/payment_item_container.dart
@@ -3,17 +3,22 @@ import 'package:e_commerce/core/utils/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
 class PaymentItemContainer extends StatelessWidget {
-  const PaymentItemContainer({super.key, required this.title, required this.child});
+  const PaymentItemContainer({super.key,  this.title, required this.child , this.hasTitle = true});
   
-  final String title;
+  final String? title;
   final Widget child;
+  final bool hasTitle ;
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(title, style: AppTextStyles.bold13,),
-        const SizedBox(height: 16,),
+        hasTitle ? Column(
+          children: [
+            Text(title!, style: AppTextStyles.bold13,),
+            const SizedBox(height: 16,),
+          ],
+        ) : const SizedBox(),
         Container(
           decoration: AppDecorations.greyBoxDecoration,
           child: child,
