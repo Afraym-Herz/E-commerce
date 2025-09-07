6412f0d11faa9e1e02573d8a7476b2a3d41f1ccb
Afraym-Herz <afraym100herz@gmail.com>
2025-09-07 21:47:17 +0300

    add provider.value to checkout view

--- lib/features/checkout/presentation/views/checkout_view.dart
+++ lib/features/checkout/presentation/views/checkout_view.dart
@@ -1,13 +1,22 @@
 import 'package:e_commerce/features/checkout/presentation/views/widgets/checkout_view_body.dart';
 import 'package:e_commerce/features/checkout/presentation/views/widgets/checkout_view_body_2.dart';
+import 'package:e_commerce/features/home/domain/entites/cart_entity.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CheckoutView extends StatelessWidget {
-  const CheckoutView({super.key});
+  const CheckoutView({super.key, required this.cartEntity});
   static const routeName = 'checkout';
+
+  final CartEntity cartEntity;
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: CheckoutViewBody2() , backgroundColor: Colors.white,);
+    return  Scaffold(
+      body: Provider.value(
+        value: cartEntity ,
+        child: const CheckoutViewBody2()),
+      backgroundColor: Colors.white,
+    );
   }
 }
