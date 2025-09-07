d2b8b6d870710333011914edefdf68d9cdc8a195
Afraym-Herz <afraym100herz@gmail.com>
2025-09-07 21:46:46 +0300

    add confirm section in papment and review body

--- lib/features/checkout/presentation/views/widgets/payment_and_review_body.dart
+++ lib/features/checkout/presentation/views/widgets/payment_and_review_body.dart
@@ -2,7 +2,7 @@ import 'package:e_commerce/core/utils/app_text_styles.dart';
 import 'package:e_commerce/core/utils/assets.dart';
 import 'package:e_commerce/features/checkout/presentation/views/widgets/payment_item_container.dart';
 import 'package:e_commerce/features/checkout/presentation/views/widgets/paypalInfoWidget.dart';
-import 'package:e_commerce/features/checkout/presentation/views/widgets/review_info_widget.dart';
+import 'package:e_commerce/features/checkout/presentation/views/widgets/summary_review_info_widget.dart';
 import 'package:flutter/material.dart';
 
 class PaymentAndReviewBody extends StatelessWidget {
@@ -10,28 +10,50 @@ class PaymentAndReviewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Column(
         children: [
-          PaymentItemContainer(
+          const PaymentItemContainer(
             title: 'ملخص الطلب :',
-            child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 16),
-              child: ReviewInfoWidget(),
-            ),
+            child: SummaryReviewInfoWidget(),
           ),
-          SizedBox(height: 22),
-          PaymentItemContainer(
+          const SizedBox(height: 22),
+          const PaymentItemContainer(
             title: 'يرجي تأكيد  طلبك :',
             child: PaypalInfoWidget(),
           ),
-          SizedBox(height: 8,),
-          
-          
+          const SizedBox(height: 12),
+          PaymentItemContainer(
+            hasTitle: false,
+            child: Column(
+              children: [
+                const Row(
+                  children: [
+                    Text('عنوان التوصيل', style: AppTextStyles.bold13),
+                    Spacer(),
+                    Row(
+                      children: [
+                        Icon(Icons.edit, size: 18),
+                        SizedBox(width: 6),
+                        Text('تعديل', style: AppTextStyles.semiBold13),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 12),
+                Row(
+                  children: [
+                    Image.asset(Assets.imagesLocationIcon),
+                    const SizedBox(width: 6),
+                    const Text('شارع النيل، مبنى رقم ١٢٣' , style: AppTextStyles.regular16,),
+                  ],
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
   }
 }
-
