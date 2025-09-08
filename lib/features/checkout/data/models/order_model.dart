87fd203ff267b3cb556c4c39acf2fedfcfffdc83
Afraym-Herz <afraym100herz@gmail.com>
2025-09-09 00:16:32 +0300

    edit order model

--- lib/features/checkout/data/models/order_model.dart
+++ lib/features/checkout/data/models/order_model.dart
@@ -1,27 +1,42 @@
+import 'package:e_commerce/core/helper_functions/get_saved_user_data.dart';
 import 'package:e_commerce/features/checkout/data/models/order_product_model.dart';
 import 'package:e_commerce/features/checkout/data/models/shipping_address_model.dart';
+import 'package:e_commerce/features/checkout/domain/entities/order_entity.dart';
 
 class OrderModel {
   final String uID;
-  final String payway;
+  final bool paypal;
   final double totalPrice;
   final ShippingAddressModel shippingAddressModel;
   final List<OrderProductModel> orderProducts;
 
   OrderModel({
     required this.uID,
-    required this.payway,
+    required this.paypal,
     required this.totalPrice,
     required this.shippingAddressModel,
     required this.orderProducts,
   });
 
-  toJson() => {
-        'uID': uID,
-        'payway': payway,
-        'totalPrice': totalPrice,
-        'shippingAddressModel': shippingAddressModel.toJson(),
-        'orderProducts': orderProducts.map((e) => e.toJson()).toList(),
-      };
+  factory OrderModel.fromEntity(OrderEntity orderEntity) => OrderModel(
+    uID: orderEntity.uID,
+    paypal: orderEntity.payWithCash!,
+    totalPrice: orderEntity.cartEntity.calcTotalPrice().toDouble(),
+    shippingAddressModel: ShippingAddressModel.fromEntity(
+      orderEntity.shippingAddressEntity,
+    ),
+    orderProducts: orderEntity.cartEntity.cartItems
+        .map((e) => OrderProductModel.fromCartItemEntity(e))
+        .toList(),
+  );
 
+  toJson() => {
+    'uID': uID,
+    'payway': paypal,
+    'totalPrice': totalPrice,
+    'shippingAddressModel': shippingAddressModel.toJson(),
+    'orderProducts': orderProducts.map((e) => e.toJson()).toList(),
+  };
+
+  
 }
